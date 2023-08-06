4c9a44518c639584bc78da9668e05083e99558d3
Zelinov Maksim <mzelinov@ourgapps.com>
2023-08-06 18:23:05 +0300

    Removed extra dependency

--- next.config.js
+++ next.config.js
@@ -1,29 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const copyWebpackPlugin = require('copy-webpack-plugin')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
-    // webpack: (config, { isServer }) => {
-    //     if (!isServer) {
-    //         config.plugins.push(
-    //             new copyWebpackPlugin({
-    //                 patterns: [
-    //                     {
-    //                         from: 'src/test/',
-    //                         to: 'static/test/',
-    //                     },
-    //                 ],
-    //             }),
-    //         )
-    //     }
-    //     return config
-    // },
 }
 
 module.exports = nextConfig
